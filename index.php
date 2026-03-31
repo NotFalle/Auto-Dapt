e7d3c9671bce76c42da98ce7e6cb51da3735fea1
NotFalle <110547522+NotFalle@users.noreply.github.com>
2026-03-31 10:32:14 +0000

    Co-authored-by: RemleRemle <RemleRemle@users.noreply.github.com>

--- index.php
+++ index.php
@@ -23,13 +23,13 @@ $movies = $result->fetch_all(MYSQLI_ASSOC);
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/main.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Auto-Dapt</title>
 </head>
 
 <body>
